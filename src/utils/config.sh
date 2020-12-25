fb53ad7a89419b7008396fb2ec08bdc164e54156
Florian Schroedl <flo.schroedl@gmail.com>
2020-12-25 15:56:58 +0100

    Add tall ratio

--- src/utils/config.sh
+++ src/utils/config.sh
@@ -2,7 +2,7 @@ XDG_CONF=${XDG_CONFIG_DIR:-"$HOME/.config"};
 CONFIG_DIR="$XDG_CONF/bsp-layout";
 
 # Default config
-export TALL_RATIO=0.6;
+export TALL_RATIO=0.76;
 export WIDE_RATIO=0.6;
 
 source "$CONFIG_DIR/layoutrc" 2> /dev/null || true;
